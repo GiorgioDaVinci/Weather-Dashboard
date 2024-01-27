a833a77be9bf2818a8938b8b11a346d7d7da4195
GiorgioDaVinci <okoriegeorge1@gmail.com>
2024-01-27 12:55:18 +0000

    I have added logic that displays the weather of a city when the user inputs a city name

--- starter/script.js
+++ starter/script.js
@@ -1,23 +1,34 @@
 $(document).ready(function () {
 
-  
-})
+  $("#search-form").submit(function(event){
+    event.preventDefault();
 
-function cityWeather(cityName){
-  var APIKey = "c8eb07a44e7175d7747d40acd09c89e0";
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",&limit=1&appid=" +
-  APIKey;
+    var cityName = $("#search-input").val();
 
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-
-  .then(function (data) {
-    console.log(data);
+    cityWeather(cityName);
   });
-}
-cityWeather();
+
+  function cityWeather(cityName){
+    var APIKey = "c8eb07a44e7175d7747d40acd09c89e0";
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",&limit=1&appid=" +
+    APIKey;
+  
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+  
+    .then(function (data) {
+      console.log(data);
+      updateUI(data)
+    });
+  }
+
+  function updateUI(data){}
+});
+
+
+
 
 
